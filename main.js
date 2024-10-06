2cf2a75637f4bb0d2ca7f90355dd33546bbf0480
Marlon Andrés Diaz Suarez <marlondiaz2994@gmail.com>
2024-10-05 22:35:01 -0500

    idAudio code

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0
 
 while (contador < 9 ){
-    listaDeTeclas[contador].onclick = function(){
-        playSonido();
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    
+    
+    const  idAudio = `#sonido_${instrumento}`;
+    console.log( idAudio );
+
+    tecla.onclick = function(){
+        playSonido(idAudio);
     };
     contador = contador + 1
-    console.log('Vuelta' + contador)
 }
