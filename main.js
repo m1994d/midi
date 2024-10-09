21d71434c7c30ce7bcabeab3dee528c2e0438e13
Marlon Andrés Diaz Suarez <marlondiaz2994@gmail.com>
2024-10-08 21:28:20 -0500

    fin.

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ function playSonido (idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
 
-while (contador < 9 ){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-    
-    
+        
     const  idAudio = `#sonido_${instrumento}`;
     console.log( idAudio );
 
     tecla.onclick = function(){
         playSonido(idAudio);
     };
-    contador = contador + 1
+    tecla.onkeydown = function(evento){        
+        if( evento.code === 'Space' ||  evento.code === 'Enter'){
+           tecla.classList.add('activa'); 
+        }
+        console.log(evento.code === 'Space' ||  evento.code === 'Enter')
+
+
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa');   
+    }
+        
 }
